818673cee69ed8b429c85457ed40eafd61d26ff9
MargauxThirion <thirion.margaux@gmail.com>
2024-11-15 14:56:21 +0100

    first commit

---
+++ src/main/java/com/example/iwa_ms_admin_moderation/IwaMsAdminModerationApplication.java
@@ -0,0 +1,13 @@
+package com.example.iwa_ms_admin_moderation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwaMsAdminModerationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IwaMsAdminModerationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iwa_ms_admin_moderation/IwaMsAdminModerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iwa_ms_admin_moderation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwaMsAdminModerationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
